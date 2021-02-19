201370176ccf37d13084b956b640c4ead2d48229
Kym Lee <kym.nazim@gmail.com>
2021-02-19 23:24:09 +1100

    Cocktail
    grid not working these 'nth child'

--- Patron Tequila/root/js/cocktails.js
+++ Patron Tequila/root/js/cocktails.js
@@ -1,7 +1,7 @@
 const aboutTl = gsap.timeline({
 
     scrollTrigger: {
-        trigger: ".cocktails ",
+        trigger: ".cocktails-img ",
         start: "top center",
         end: "bottom bottom",
         scrub: "true",
@@ -9,5 +9,5 @@ const aboutTl = gsap.timeline({
     }
 })
 
-aboutTl.to('.cocktail-1', { x: 900, y: 400, duration: 5, ease: "back.out(1.7)" })
-aboutTl.to('.cocktail-2', { x: -900, y: 400, duration: 5, ease: "back.out(1.7)" }, "-=1.5")
+aboutTl.to('.cocktail3', { x: 900, y: 400, duration: 5, ease: "back.out(1.7)" })
+aboutTl.to('.cocktail-4', { x: -900, y: 400, duration: 5, ease: "back.out(1.7)" }, "-=1.5")
--- Patron Tequila/root/js/main.js
+++ Patron Tequila/root/js/main.js
@@ -2,7 +2,9 @@ const tlHero = gsap.timeline()
 
 tlHero.from('#hero-text, h1', { duration: 2.5, ease: "back.out(1.6)", x: -500 })
 tlHero.from('.hero-image', { y: -500, duration: 2, ease: "bounce" }, "-=2.5")
-tlHero.from('.modal-intro', 'yes', 'no', {
+tlHero.from('.modal-intro', {
+    '.yes'
+    '.no',
     x: -500,
     duration: 4,
     ease: "back.out(1.5)",
